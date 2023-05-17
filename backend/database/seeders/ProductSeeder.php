add864d72ad838577b73ac87a4d80d6615cf6e93
Alan Sanchez <alaansannchezz@gmail.com>
2023-05-17 15:02:35 -0300

    Se añadieron imagenes a los Products Seeders

--- backend/database/seeders/ProductSeeder.php
+++ backend/database/seeders/ProductSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Image;
 use App\Models\Product;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class ProductSeeder extends Seeder
 {
@@ -13,6 +14,16 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        Product::factory(50)->create();
+        Product::factory(50)
+            ->create()
+            ->each(
+                fn ($product) =>
+                Image::factory(3)
+                    ->create([
+                        'url' => 'products/' . fake()->image('public/storage/products', 640, 480, null, false),
+                        'imageable_id' => $product->id,
+                        'imageable_type' => Product::class
+                    ])
+            );
     }
 }
