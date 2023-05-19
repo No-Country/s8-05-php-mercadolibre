e1cc2852beaf18cc56b39b3d419690bebacad648
Felipe Suarez <felipesuarez1525@gmail.com>
2023-05-19 11:00:51 -0300

    add prettier config

--- frontend/src/utils/apiClient.ts
+++ frontend/src/utils/apiClient.ts
@@ -1,15 +1,15 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const apiClient = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: 'http://localhost:8000/api',
 });
 
 const apiClientPriv = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: 'http://localhost:8000/api',
 });
 
 apiClientPriv.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token');
 
   if (!token) return config;
 
