59ad6a5d9f2ad8ab39743209842293187680c1ab
Felipe Suarez <felipesuarez1525@gmail.com>
2023-05-19 13:02:35 -0300

    update sizes in register

--- frontend/src/Components/Register/Card.tsx
+++ frontend/src/Components/Register/Card.tsx
@@ -3,6 +3,7 @@
 import Link from 'next/link';
 
 import { MdOutlineModeEditOutline } from 'react-icons/md';
+import { BsCheckLg } from 'react-icons/bs';
 
 type CardProps = {
   title: string;
@@ -18,16 +19,21 @@ export default function Card({ title, subTitle, icon, completed, route, currentS
     <div
       className={`${
         completed ? 'bg-skyBlue' : 'bg-white border-black border'
-      } flex flex-row gap-5 bg-white justify-left items-center px-5 py-2 h-[90px] rounded-lg`}
+      } flex flex-row gap-5 bg-white justify-left items-center px-5 py-2 min-h-[90px] rounded-lg`}
     >
       <div className="text-3xl text-black">{icon}</div>
-      <div className="flex flex-row justify-between w-full items-center">
+      <div className="flex flex-row justify-between w-full items-center h-unset">
         <div className="flex flex-col">
           <h2>{title}</h2>
-          <span className="text-[13px] opacity-60">{subTitle}</span>
+          <div className="flex gap-2">
+            {completed && <BsCheckLg className="text-[12px] mt-1" />}
+            <span className="text-[13px] opacity-60 max-w-[150px] break-words whitespace-pre-wrap">
+              {subTitle}
+            </span>
+          </div>
         </div>
         {completed ? (
-          <Link href={route} className="w-[75px] ml-3 py-1 px-3 rounded-full flex justify-center">
+          <Link href={route} className="w-[75px] py-1 px-3 rounded-full flex justify-center">
             <MdOutlineModeEditOutline className="text-black text-xl" />
           </Link>
         ) : (
