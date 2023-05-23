72ef55c9e89c41553bf5b3c7c7807aa0da97d290
Felipe Suarez <felipesuarez1525@gmail.com>
2023-05-23 17:24:07 -0300

    add carousel img file in assets

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,16 +5,9 @@ import Layout from '@/Components/Layout';
 import CategorySlider from '@/Components/UI/CategorySlider';
 import { apiClient } from '@/utils/apiClient';
 import { useEffect, useState } from 'react';
+import { imgs } from '@/assets/carousel';
 
 export default function Home() {
-  const imgs = [
-    'https://http2.mlstatic.com/storage/splinter-admin/o:f_webp,q_auto:best/1683838084716-home-sliderdesktop2.jpg',
-    'https://http2.mlstatic.com/storage/splinter-admin/o:f_webp,q_auto:best/1683772259953-ms-desktop-extendimos-hotsale-cuotas.jpg',
-    'https://http2.mlstatic.com/storage/splinter-admin/o:f_webp,q_auto:best/1683821025709-home-sliderdesktop-sinfijas-peqelectro.jpg',
-    'https://http2.mlstatic.com/storage/splinter-admin/o:f_webp,q_auto:best/1683821125894-home-sliderdesktop1-full.jpg',
-    'https://http2.mlstatic.com/storage/splinter-admin/o:f_webp,q_auto:best/1683832284499-msdesktop-samsung-hs.jpg',
-  ];
-
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
