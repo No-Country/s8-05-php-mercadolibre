65819c3c759e9048edd381c9c924f8a5b2a82c73
Yenifer Ramirez <yeniferramirez11@gmail.com>
2023-05-23 23:10:40 -0400

    Login con apiClient

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,116 +1,82 @@
 'use client';
 
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { Badge, Avatar } from 'flowbite-react';
 import { apiClient } from '@/utils/apiClient';
 
+
 const Login = () => {
-  const [step, setStep] = useState(1);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [isEmailTouched, setIsEmailTouched] = useState(false);
+  const [error, setError] = useState('');
+  const router = useRouter();
 
-  const handleContinue = () => {
-    if (step === 1) {
-      setStep(2);
-    } else if (step === 2) {
-      apiClient
-        .post('/login', { email, password })
-        .then((data) => console.log(data))
-        .catch((err) => console.log(err));
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await apiClient.post('/login', { email, password });
+      // Aquí puedes procesar la respuesta de la API y realizar acciones como guardar el token de acceso en el estado global o en una cookie
+      console.log(response.data);
+      // Redirigir a la página principal después del inicio de sesión exitoso
+      router.push('/');
+    } catch (error) {
+      // Manejar errores de la API y mostrar un mensaje de error al usuario
+      setError('Credenciales incorrectas. Por favor, intenta nuevamente.');
     }
   };
 
-  const isEmailValid = email.trim() !== '';
-  const isPasswordValid = password.trim() !== '';
-  const showError = !isEmailValid && isEmailTouched;
-
-  const handleEmailBlur = () => setIsEmailTouched(true);
-
   return (
-    <div className="container flex items-center justify-center min-h-screen px-6 mx-auto">
-      <form className="w-full max-w-md">
-        <h1 className="mt-3 text-2xl text-center font-semibold capitalize sm:text-3xl">
-          Mercado Country
-        </h1>
 
-        {step === 1 && (
-          <>
-            <h2 className="mt-6 text-xl font-semibold sm:text-2xl">
-              Ingresa tú email, teléfono o usuario
-            </h2>
+    <div className="container w-full max-w-md items-center justify-center min-h-screen px-6 mx-auto">
+      
+      <h1 className="mt-3 text-2xl text-center font-semibold capitalize sm:text-3xl">
+        Mercado Country
+      </h1>
 
-            <div className="relative flex items-center mt-8">
-              <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                onBlur={handleEmailBlur}
-                className="block w-full py-3 text-gray-700 bg-white border border-neutral-400 rounded-lg focus:border-neutral-700 focus:ring-0"
-                placeholder="Escribe tu e-mail, teléfono o usuario"
-              />
-            </div>
-            {showError && (
-              <div className="text-center">
-                <p className="text-red-500 mt-2 text-xs">
-                  Por favor, ingresa un correo electrónico válido.
-                </p>
-              </div>
-            )}
-          </>
-        )}
+      <h1 className="mt-6 text-xl font-semibold sm:text-2xl">
+        Ingresa tú email, teléfono o usuario
+      </h1>
+        <form>
+        <div className="relative flex items-center mt-8">
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="block w-full py-3 text-gray-700 bg-white border border-neutral-400 rounded-lg focus:border-neutral-700 focus:ring-0"
+            placeholder="Escribe tu e-mail, teléfono o usuario"
+          />
+        </div>
 
-        {step === 2 && (
-          <>
-            <h2 className="mt-6 text-xl font-semibold sm:text-2xl">Contraseña</h2>
+        <h2 className="mt-6 text-xl font-semibold sm:text-2xl">Contraseña</h2>
 
-            <div className="flex flex-wrap gap-2">
-              <Avatar rounded={true} size="xs" />
-              <p className="mt-2 text-xs font-semibold">{email}</p>
-            </div>
+        <div className="relative flex items-center mt-4">
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="block w-full py-3 text-gray-700 bg-white border border-neutral-400 rounded-lg focus:border-neutral-700 focus:ring-0"
+            placeholder="Contraseña"
+          />
+        </div>
 
-            <div className="relative flex items-center mt-4">
-              <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="block w-full py-3 text-gray-700 bg-white border border-neutral-400 rounded-lg focus:border-neutral-700 focus:ring-0"
-                placeholder="Contraseña"
-              />
-            </div>
-          </>
+        {error && (
+          <div className="text-center">
+            <p className="text-red-500 mt-2 text-xs">{error}</p>
+          </div>
         )}
 
         <div className="mt-6">
-          {step === 1 && (
-            <button
-              onClick={handleContinue}
-              disabled={!isEmailValid}
-              className={`w-full px-6 py-4 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue rounded-3xl ${
-                !isEmailValid
-                  ? 'opacity-50 rounded-3xl cursor-not-allowed'
-                  : 'hover:bg-blue-700 rounded-3xl'
-              } focus:outline-none focus:ring focus:ring-blue focus:ring-opacity-50 rounded-3xl`}
-            >
-              Continuar
-            </button>
-          )}
-          {step === 2 && (
-            <button
-              onClick={handleContinue}
-              disabled={!isPasswordValid}
-              className={`w-full px-6 py-4 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue rounded-3xl ${
-                !isEmailValid
-                  ? 'opacity-50 rounded-3xl cursor-not-allowed'
-                  : 'hover:bg-blue-700 rounded-3xl'
-              } focus:outline-none focus:ring focus:ring-blue focus:ring-opacity-50 rounded-3xl`}
-            >
-              Iniciar sesión
-            </button>
-          )}
+          <button
+            onClick={handleLogin}
+            className="w-full px-6 py-4 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue rounded-3xl hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue focus:ring-opacity-50 rounded-3xl"
+          >
+            Iniciar sesión
+          </button>
           <div className="mt-6 text-center">
             <a href="/register" className="text-sm text-blue-500 hover:underline">
-              ¿No tienes cuenta?, Registrate
+              ¿No tienes cuenta? Regístrate
             </a>
           </div>
         </div>
@@ -120,3 +86,4 @@ const Login = () => {
 };
 
 export default Login;
+
