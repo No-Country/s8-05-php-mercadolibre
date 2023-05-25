b20a21b9dfcacdbce58d649b58b6e4ca52b15321
Yenifer Ramirez <yeniferramirez11@gmail.com>
2023-05-24 22:26:02 -0400

    Maquetado de Categoria y subcategoria

--- frontend/src/app/subcategory/page.tsx
+++ frontend/src/app/subcategory/page.tsx
@@ -5,7 +5,7 @@ import Layout from '@/Components/Layout';
 import cardImg from '@/assets/card/tecnologia.png';
 import CardTitle from '@/Components/UI/CardTitle';
 import CardImg from '@/Components/UI/CardImg';
-import CardCategory from '@/Components/UI/CardCategory';
+import CardSubCategory from '@/Components/UI/CardSubCategory';
 import SliderLogos from '@/Components/UI/SliderLogos';
 
 import { useEffect } from 'react';
@@ -22,22 +22,22 @@ export default function Category() {
   return (
     <Layout>
       <div className="flex flex-col gap-5 my-5">
-        <CardTitle title={'Tecnología'} />
-        
+        <CardTitle title={'Celulares y smartphones'} />
+        <SliderLogos />
         <div className="flex flex-wrap mx-5">
           {
             Array.from({ length: 4 }).map((_, index) => (
-              <CardCategory
+              <CardSubCategory
                 key={index}
-                title={'Celulares y telefonos'}
+                title={'Apple iPhone 14 Pro Max, LTPO Super Retina XDR OLED 6.7"'}
                 img={cardImg}
-                offer={true}
-                descriptionOffer = {'20%'}
+                leave={'Lleva hoy'}
+                store = {'Apple Oficial'}
+                price = {'131,999.00'}
               />
             ))
           }
         </div>
-        <SliderLogos />
         <CardImg
           title={'REACONDICIONADOS'}
           subTitle={'Productos testeados y con garantía'}
