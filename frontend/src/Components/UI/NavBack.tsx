814c6d4f4203ef5048b5feb2a925087a09e2c920
Felipe Suarez <felipesuarez1525@gmail.com>
2023-05-31 19:56:12 -0300

    update navBack component

--- frontend/src/Components/UI/NavBack.tsx
+++ frontend/src/Components/UI/NavBack.tsx
@@ -1,17 +1,21 @@
+import { useRouter } from 'next/navigation';
 import { FiArrowLeft } from 'react-icons/fi';
 
 type NavBackProps = {
   title: string;
+  callback?: () => void;
 };
 
-export default function NavBack({
-  title
-}: NavBackProps) {
+export default function NavBack({ title, callback }: NavBackProps) {
+  const { back } = useRouter();
+
+  const handleClick = () => (callback ? callback() : back());
+
   return (
     <nav className="flex items-center justify-between p-4 bg-white text-neutral-700 border-b border-1">
       <div className="flex items-center">
         <button
-          onClick={() => window.history.back()}
+          onClick={handleClick}
           className="flex items-center justify-center w-8 h-8 text-neutral-700 rounded-full hover:bg-blue focus:outline-none"
         >
           <FiArrowLeft size={20} />
