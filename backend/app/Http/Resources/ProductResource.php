c7ce6fb0501088fd31ddf5864d19951506d3c848
Tu usuario de GitHub aca <Tu email aca>
2023-05-31 15:54:07 -0500

    se agrego el carrito y los test

--- backend/app/Http/Resources/ProductResource.php
+++ backend/app/Http/Resources/ProductResource.php
@@ -22,6 +22,7 @@ class ProductResource extends JsonResource
                 'description' => $this->description,
                 'price' => $this->price,
                 'stock' => $this->stock,
+                'measures' => $this->measures,
             ],
             'relationships' => [
                 'images' => $this->images->pluck('url'),
