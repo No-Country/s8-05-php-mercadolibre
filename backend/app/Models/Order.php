2eb022f352f6d2a1628b6c23f6815b2cc94396a9
Alan Sanchez <alaansannchezz@gmail.com>
2023-06-02 17:34:36 -0300

    chore: se realizaron pequeños cambios en la tabla orders

--- backend/app/Models/Order.php
+++ backend/app/Models/Order.php
@@ -4,20 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\User;
-use App\Models\{City, State};
+use App\Models\{City, State, User};
 
 class Order extends Model
 {
     use HasFactory;
 
-    protected $guarded = ['id', 'created_at', 'updated_at'];
+    // Columna  =>  'dispatch_address'
+    const DOMICILIO     = 'A DOMICILIO';
+    const SUCURSAL      = 'RETIRO EN SUCURSAL';
+
+    // Columna  =>  'status'
+    const PENDIENTE     = 'PENDIENTE';
+    const RECIBIDO      = 'RECIBIDO';
+    const ENVIADO       = 'ENVIADO';
+    const ENTREGADO     = 'ENTREGADO';
+    const CANCELADO     = 'CANCELADO';
+
+    protected $guarded = ['id', 'status', 'created_at', 'updated_at'];
 
     public function city()
     {
         return $this->belongsTo(City::class);
     }
-    
+
     public function state()
     {
         return $this->belongsTo(State::class);
