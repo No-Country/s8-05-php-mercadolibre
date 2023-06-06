9ec38ad6f39d98c3806c5dc61a69260f0ec7f754
Felipe Suarez <felipesuarez1525@gmail.com>
2023-06-06 11:23:47 -0300

    add update and remove conections

--- frontend/src/Components/Product/CartProduct.tsx
+++ frontend/src/Components/Product/CartProduct.tsx
@@ -1,6 +1,30 @@
+'use client';
+
+import { apiClient } from '@/utils/apiClient';
 import Image from 'next/image';
+import { useState } from 'react';
 
 export default function CartProduct() {
+  const [count, setCount] = useState(1);
+
+  const handleCountRest = (id: string) => {
+    if (count > 1) {
+      setCount(count - 1);
+      apiClient.put(`/update-cart`, { product_id: id, quantity: count });
+    }
+  };
+
+  const handleCountSum = (id: string) => {
+    if (count < 10) {
+      setCount(count + 1);
+      apiClient.put(`/update-cart`, { product_id: id, quantity: count });
+    }
+  };
+
+  const handleDelete = (id: string) => {
+    apiClient.delete(`/remove-cart/${id}`);
+  };
+
   return (
     <div className="flex flex-col mx-5 shadow-lg">
       <div className="flex flex-row border-b border-black gap-2">
@@ -8,14 +32,16 @@ export default function CartProduct() {
         <div className="flex flex-col w-full gap-2 justify-between pr-2">
           <p className="text-lg text-darkBlue">Nombre del producto</p>
           <div className="flex flex-row gap-2 w-24 justify-between px-2 border border-black rounded-full">
-            <button> - </button>
-            <span>1 u.</span>
-            <button> + </button>
+            <button onClick={() => handleCountRest('1')}> - </button>
+            <span>{count} u.</span>
+            <button onClick={() => handleCountSum('1')}> + </button>
           </div>
           <span className="self-end text-sm font-semibold">AR $ 1000</span>
         </div>
       </div>
-      <span className="text-darkBlue text-lg ml-5 py-1">Eliminar</span>
+      <span className="text-darkBlue text-lg ml-5 py-1" onClick={() => handleDelete('1')}>
+        Eliminar
+      </span>
     </div>
   );
 }
--- frontend/src/app/cart/page.tsx
+++ frontend/src/app/cart/page.tsx
@@ -3,18 +3,40 @@
 import Buttons from '@/Components/Cart/Buttons';
 import CartProduct from '@/Components/Product/CartProduct';
 import NavBack from '@/Components/UI/NavBack';
+import { apiClient } from '@/utils/apiClient';
+import Link from 'next/link';
+
+// async function getData() {
+//   const { data } = await apiClient.get('/view-cart');
+//   return data;
+// }
 
 export default function Page() {
+  // const { data } = await getData();
+  // console.log(data);
+  const data = [1, 1];
+
   return (
     <>
       <NavBack title={'Carrito de compras'} />
-      <div className="flex flex-col gap-5 my-5">
-        <CartProduct />
-        <CartProduct />
-        <button>Vaciar carrito</button>
-      </div>
-
-      <Buttons />
+      {data.length ? (
+        <>
+          <div className="flex flex-col gap-5 my-5 min-h-[50vh]">
+            {data.map((item, index) => (
+              <CartProduct key={index} />
+            ))}
+            <button>Vaciar carrito</button>
+          </div>
+          <Buttons />
+        </>
+      ) : (
+        <div className="flex flex-col m-5 gap-5 mt-[30vh]">
+          <h1 className="text-center text-xl">Tu carrito esta vacio!</h1>
+          <Link href={'/'} className="bg-darkBlue text-white rounded-full py-3 text-center">
+            Ir a comprar
+          </Link>
+        </div>
+      )}
     </>
   );
 }
