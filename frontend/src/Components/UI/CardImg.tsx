db0884813ea14f00de72d5b8591ccf36abd94079
Yenifer Ramirez <yeniferramirez11@gmail.com>
2023-06-08 11:42:41 -0400

    responsive category

--- frontend/src/Components/UI/CardImg.tsx
+++ frontend/src/Components/UI/CardImg.tsx
@@ -8,7 +8,7 @@ type CardImgProps = {
 
 export default function CardImg({ title, subTitle, img }: CardImgProps) {
   return (
-    <div className="flex flex-row justify-between bg-card rounded-lg p-5 gap-2 items-center mx-5">
+    <div className="flex flex-row justify-between bg-card rounded-lg p-5 gap-2 items-center mx-5 md:h-64 h-36">
       <div className="text-white flex flex-col gap-5 pb-5">
         <span className="text-xs font-bold">{title}</span>
         <p className="text-lg font-light">{subTitle}</p>
@@ -16,9 +16,9 @@ export default function CardImg({ title, subTitle, img }: CardImgProps) {
       <Image
         src={img}
         width={200}
-        height={200}
+        height={100}
         alt="Imagen de tecnologia"
-        className="object-cover w-3/6 h-full"
+        className="object-cover h-auto"
       />
     </div>
   );
